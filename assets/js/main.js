3bebf87f6f81a89082eeb75faae56dd9f39bb4bd
vladahvala <80634246+vladahvala@users.noreply.github.com>
2025-07-16 01:02:31 +0300

    Scroll reveal animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,3 +68,36 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 300,
+    //reset: true, //Animations repeat
+})
+
+sr.reveal(`.home__data, .about__data, .footer__container`)
+sr.reveal(`.home__images`, {delay: 1000})
+sr.reveal(`.home__dam-1`, {delay: 1000, scale: 0, rotate: {z: 45}})
+sr.reveal(`.home__dam-2 , .home__dam-3`, {delay: 1400, scale: 0, rotate: {z: 45}})
+sr.reveal(`.home__dam-4`, {delay: 1700, scale: 0, rotate: {z: 45}})
+sr.reveal(`.about__img-1`, {delay: 600, origin: 'right'})
+sr.reveal(`.about__img-2`, {delay: 800, origin: 'left'})
+sr.reveal(`.about__tooltip-1`, {delay: 1600, origin: 'left'})
+sr.reveal(`.about__tooltip-2`, {delay: 1800, origin: 'right'})
+sr.reveal(`.order__card:nth-child(1)`, {origin: 'right'})
+sr.reveal(`.order__card:nth-child(2)`, {origin: 'left'})
+sr.reveal(`.about__tooltip-1`, {delay: 800, origin: 'right'})
+sr.reveal(`.about__tooltip-2`, {delay: 800, origin: 'left'})
+sr.reveal(`.combo__img`, {delay: 1000})
+sr.reveal(`.combo__titles`, {origin: 'bottom'})
+sr.reveal(`.combo__data`, {delay: 1600})
+sr.reveal(`.combo__numbers`, {delay: 1600, origin: 'bottom'})
+sr.reveal(`.contact__data`, {origin: 'right'})
+sr.reveal(`.contact__info`, {origin: 'left'})
+sr.reveal(`.contact__img-1`, {delay: 1000, distance: 0, scale: 0, rotate: {z:-45}})
+sr.reveal(`.contact__img-2`, {delay: 1200, distance: 0, scale: 0, rotate: {z:45}})
+sr.reveal(`.contact__dam-1`, {delay: 1400, scale: 0, rotate: {z:45}})
+sr.reveal(`.contact__dam-3`, {delay: 1600, scale: 0, rotate: {z:45}})
+sr.reveal(`.contact__dam-2`, {delay: 1800, scale: 0, rotate: {z:45}})
+sr.reveal(`.contact__dam-4`, {delay: 2000, scale: 0, rotate: {z:45}})
